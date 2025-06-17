8a6fa988bea62dd793a41907be926520662e73b8
Ferran Enguix <ferranet@ext.inditex.com>
2025-06-17 19:29:32 +0200

    feat: added Home to Routes to have minimal navigation

--- src/components/Index/Subcomponents/Header.jsx
+++ src/components/Index/Subcomponents/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -14,7 +15,21 @@ const Header = () => {
           alignItems: "center",
         }}
       >
-        <h1 style={{ margin: 0, textAlign: "left" }}> Demo Streaming </h1>
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <h1 style={{ margin: 0, textAlign: "left" }}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="#fff"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 2 7.5V14a1 1 0 0 0 1 1h3.5a.5.5 0 0 0 .5-.5V11a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v3.5a.5.5 0 0 0 .5.5H13a1 1 0 0 0 1-1V7.5a.5.5 0 0 0-.146-.354l-6-6z" />
+            </svg>{" "}
+            Demo Streaming
+          </h1>
+        </Link>
+
         <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
           <span style={{ color: "white", fontWeight: "bold" }}>Login</span>
           <button
