ec2febd557813d9f0e7cced96bfa0b9a6f00679d
Ferran Enguix <ferranet@ext.inditex.com>
2025-06-17 15:58:26 +0200

    feat: proper images on footer

--- src/components/Index/Subcomponents/Footer.jsx
+++ src/components/Index/Subcomponents/Footer.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+import facebookImg from "../assets/social/facebook-white.svg";
+import twitterImg from "../assets/social/twitter-white.svg";
+import instagramImg from "../assets/social/instagram-white.svg";
+
+import appStoreImg from "../assets/store/app-store.svg";
+import playStoreImg from "../assets/store/play-store.svg";
+import windowsStoreImg from "../assets/store/windows-store.svg";
+
 const footerLinks = [
   "Home",
   "Terms and Conditions",
@@ -8,6 +16,17 @@ const footerLinks = [
   "Help",
   "Manage Account",
 ];
+
+const SocialImage = ({ src, alt, size = 32 }) => (
+  <img
+    src={src}
+    alt={alt}
+    style={{
+      width: size,
+      height: size,
+    }}
+  />
+);
 
 const Footer = () => (
   <footer
@@ -37,105 +56,32 @@ const Footer = () => (
     >
       Copyright @ 2016 DEMO Streaming. All Rights Reserved
     </div>
-    <ul
+    <div
       style={{
-        listStyle: "none",
         display: "flex",
-        justifyContent: "center",
-        gap: "16px",
-        padding: 0,
-        margin: 0,
+        alignItems: "center",
+        width: "100%",
+        marginTop: "1 rem",
       }}
     >
-      <li>
-        <a
-          href="https://facebook.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Facebook"
-        >
-          <img
-            src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/facebook.svg"
-            alt="Facebook"
-            style={{
-              width: 32,
-              height: 32,
-              display: "block",
-              backgroundColor: "#fff",
-              padding: 6,
-              borderRadius: "50%",
-              boxShadow: "0 1px 4px rgba(0,0,0,0.08)",
-            }}
-          />
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Twitter"
-        >
-          <img
-            src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/twitter.svg"
-            alt="Twitter"
-            style={{
-              width: 32,
-              height: 32,
-              display: "block",
-              backgroundColor: "#fff",
-              padding: 6,
-              borderRadius: "50%",
-              boxShadow: "0 1px 4px rgba(0,0,0,0.08)",
-            }}
-          />
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://instagram.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Instagram"
-        >
-          <img
-            src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/instagram.svg"
-            alt="Instagram"
-            style={{
-              width: 32,
-              height: 32,
-              display: "block",
-              backgroundColor: "#fff",
-              padding: 6,
-              borderRadius: "50%",
-              boxShadow: "0 1px 4px rgba(0,0,0,0.08)",
-            }}
-          />
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://linkedin.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="LinkedIn"
-        >
-          <img
-            src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/linkedin.svg"
-            alt="LinkedIn"
-            style={{
-              width: 32,
-              height: 32,
-              display: "block",
-              backgroundColor: "#fff",
-              padding: 6,
-              borderRadius: "50%",
-              boxShadow: "0 1px 4px rgba(0,0,0,0.08)",
-            }}
-          />
-        </a>
-      </li>
-    </ul>
+      <div style={{ display: "flex", gap: "8px" }}>
+        <SocialImage src={facebookImg} alt="Facebook" size={30} />
+        <SocialImage src={twitterImg} alt="Twitter" size={30} />
+        <SocialImage src={instagramImg} alt="Instagram" size={30} />
+      </div>
+      <div style={{ display: "flex", gap: "8px", marginLeft: "auto" }}>
+        <img src={appStoreImg} alt={"appStoreImg"} />
+        <img src={playStoreImg} alt={"appStoreImg"} />
+        <img
+          src={windowsStoreImg}
+          alt={"appStoreImg"}
+          style={{
+            width: 110,
+            height: 150,
+          }}
+        />
+      </div>
+    </div>
   </footer>
 );
 
